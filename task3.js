2e71d6585e398529ff979a01b4379286ce274e5c
Sagor8187 <sdsagor8187@gmail.com>
2026-01-28 23:43:46 +0600

    task 3 solve

--- task3.js
+++ task3.js
@@ -1,4 +1,12 @@
 // Task-3:
 // Check whether a string contains all the vowels a, e, i, o, u
 
-let 
+let text = "hello".toLowerCase()
+let vowel = 0
+
+for( let i = 0 ; i < text.length;i++){
+    if (text[i] === 'a' || text[i] === 'e' || text[i] === 'i' || text[i] === 'o' || text[i] === 'u'){
+        vowel +=1
+    }
+}
+console.log(vowel)
